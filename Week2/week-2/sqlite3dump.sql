f71be8d7262873422fb6b1be3612bb471b68d1f8
devpatel658 <139364049+devpatel658@users.noreply.github.com>
2023-08-16 13:29:14 +0530

    Add files via upload

---
+++ Week2/week-2/sqlite3dump.sql
@@ -0,0 +1,12 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE album1
+(
+id int,
+name varchar,
+city varchar
+);
+INSERT INTO album1 VALUES(1,'dev','surat');
+INSERT INTO album1 VALUES(2,'dhruv','bardoli');
+INSERT INTO album1 VALUES(3,'darshan','surat');
+COMMIT;
